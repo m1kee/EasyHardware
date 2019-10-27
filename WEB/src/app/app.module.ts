ba8c2b5a1823dd6fcf96479e9b4c82fa27a9e154
M1kee <michael.nunezb@outlook.com>
2019-10-27 15:16:29 -0300

    added subcategories, and product list (just html)

--- WEB/src/app/app.module.ts
+++ WEB/src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/categories/menu/menu.component';
 import { CategoryComponent } from './components/categories/category/category.component';
 
 import { CategoryService } from './components/categories/category.service';
+import { ProductComponent } from './components/products/product/product.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+
 
 @NgModule({
     declarations: [
         AppComponent,
         MenuComponent,
-        CategoryComponent
+        CategoryComponent,
+        ProductComponent,
+        ProductListComponent
     ],
     imports: [
         BrowserModule,
-        HttpClientModule
+        HttpClientModule,
+        FontAwesomeModule,
+        BrowserAnimationsModule,
+        CollapseModule.forRoot(),
+        BsDropdownModule.forRoot()
     ],
     providers: [CategoryService],
     bootstrap: [AppComponent]
--- WEB/src/app/components/categories/menu/menu.component.ts
+++ WEB/src/app/components/categories/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../category.service';
+import { faShoppingCart, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu',
@@ -9,10 +10,22 @@ import { CategoryService } from '../category.service';
 export class MenuComponent implements OnInit {
 
     categories: any = [];
+    faShoppingCart: IconDefinition = faShoppingCart;
+    shoppingCart: any = {
+        items: []
+    }
 
     constructor(private categoryService: CategoryService) { }
 
     ngOnInit() {
+        // TODO: get shopping cart items from localstorage
+        this.shoppingCart.items.push('Notebook Gamer');
+        this.shoppingCart.items.push('PC Gamer');
+        this.shoppingCart.items.push('SSD 512 GB');
+        this.shoppingCart.items.push('NVIDIA 2080TI');
+
+        // TODO: put categories in the localstorage to not go to the API
+        // getting categories
         this.categoryService.getAll().subscribe((categories: any[]) => {
             console.log('categories: ', categories);
             this.categories = categories;
